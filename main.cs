c8932be3eb2f56aa682ece10abe6ad29a1def698
Serge Antonenko <93328309+MrEnglishCat@users.noreply.github.com>
2021-12-27 18:40:37 +0000

    first example is check

---
+++ main.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+// 1) Метод, который выводит на экран строку. Символы, из которых состоит строка, и их количество вводятся пользователем. 
+// 2) Написать программу для поиска индекса элемента массива (тип элементов в массиве -int; ). Поиск должен вернуть индекс первого найденного элемента. 
+
+  //////////////////////////////////////
+       /* int countOfUserSymbol = 0;
+        string symbolOfUser = "";
+        Console.Write("Введите количество символов в строке: ");
+        countOfUserSymbol = int.Parse(Console.ReadLine());
+        Console.Write("Ввкдите символ из которого будет состоять строка: ");
+        symbolOfUser = Console.ReadLine();
+        for (int i = 1; i <= countOfUserSymbol; i++)
+        {
+            Console.Write ($@" {symbolOfUser} ");
+        }   
+        */
+
+        /////////////////////////////////////
+
+        
+    }
+}
