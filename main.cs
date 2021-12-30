d8bf1228753f01a98bad504ce64c49d2d2d5effa
Serge Antonenko <93328309+MrEnglishCat@users.noreply.github.com>
2021-12-30 18:58:49 +0000

    OutArray - don't work!

--- main.cs
+++ main.cs
@@ -3,9 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 public class Program
 {
+    
+    static int[] GetArray (int length, int minValue, int maxValue)
+    {
+     Random random = new Random();
+     int[] array_1 = new int[length];      
+
+     for (int i = 0; i < array_1.Length; i++)
+     {
+         array_1[i] = random.Next(minValue,maxValue);
+     }    
+     return array_1;
+    }
+
+    static int[] OutArray (int[] array_1)
+    {
+     
+       for (int i = 0; i < array_1.Length; i++)
+        {
+            Console.Write ($"\t{array_1}");
+        }
+     return array_1;
+    }
+    
+    
     public static void Main()
     {
-// 1) Метод, который выводит на экран строку. Символы, из которых состоит строка, и их количество вводятся пользователем. 
+// +1) Метод, который выводит на экран строку. Символы, из которых состоит строка, и их количество вводятся пользователем. 
 // 2) Написать программу для поиска индекса элемента массива (тип элементов в массиве -int; ). Поиск должен вернуть индекс первого найденного элемента. 
 
   //////////////////////////////////////
@@ -22,7 +46,28 @@ public class Program
         */
 
         /////////////////////////////////////
+        int search = 0;
+        bool check = false;
+        int[] myArray =  GetArray(-10, 10, 10);
+        OutArray (myArray);
 
-        
+     
+     Console.Write("Введите элемент для поиска: ");
+     search = int.Parse(Console.ReadLine());
+
+     for (int i = 0; i < myArray.Length; i++)
+     {
+        if (search == myArray[i])
+        {
+          Console.WriteLine($"Элемент найден. Индекс элемента {search} - {i}");         
+          check = true;
+           break;
+        }
+           
+     }  
+     if (check == false)
+     {
+         Console.WriteLine($"Элемент не найден.");
+     }
     }
 }
